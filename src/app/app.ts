d5c548809ea1e0aede34b423486d2554845ef582
Divyanka Saxena <divyankasaxena688@gmail.com>
2026-07-22 21:58:57 +0530

    Added signup component and updated app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, RouterOutlet  ],
+  imports: [Login, Signup, RouterOutlet  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-   title = 'Learning Angular';
-   name = 'Divyanka Saxena';
-
-   x=10;
-   
+  count = 0 
+  handleIncrement() {
+    this.count = this.count + 1;
+  }
+  handleDecrement() {
+    this.count = this.count - 1;
+  }
+  handleReset() {
+    this.count = 0;
+  }
 }
